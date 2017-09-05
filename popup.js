e52f198116776aca76a59d53e45de803e5c18235
JolinJiang <31231896+JolinJiang@users.noreply.github.com>
2017-09-05 14:43:19 +0800

    Initial commit
    This is a toy programe which were cloned from a beauty girl.

---
+++ popup.js
@@ -0,0 +1,111 @@
+var messagesRegex = /^https?:\/\/www.facebook.com\/messages\/(.*)/;
+var messengerRegex = /^https?:\/\/www.messenger.com\/t\/(.*)/;
+
+function getCurrentTabUrl(callback) {
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    callback(tabs[0]);
+  });
+}
+
+function renderAtId(id, text) {
+  document.getElementById(id).innerHTML = text;
+}
+
+function renderAtAllClasses(className, text) {
+  Array.from(document.getElementsByClassName(className)).forEach(function(e) {
+    e.innerHTML = text;
+  });
+}
+
+function formatDate(rawDate) {
+  return Math.round(rawDate / 1000 / 60 * 100) / 100;
+}
+
+function displayCurConvoInfo(e) {
+  if (!e) {
+    renderAtId('error', 'Oops something went wrong. Please refresh and try again :(');
+    return;
+  }
+  renderAtId('error', '');
+
+  renderAtId('status', 'Out of ' + e.messageNumber + ' most recent messages with ' + e.oppName + ':');
+  renderAtAllClasses('name_opp', e.oppName);
+
+  var cntSession = 0;
+  var last = -1;
+  // 0: self, 1: opp
+  var totReplyTime = [0, 0];
+  var lastReplyTimestamp = [0, 0];
+  var totMsgNumber = [0, 0];
+
+  for (var i in e.messages) {
+    var m = e.messages[i];
+    if (m.type === 'separator') {
+      // A separator means a start of a convo session. Restart timestamp.
+      lastReplyTimestamp[0] = 0;
+      lastReplyTimestamp[1] = 0;
+      last = -1;
+    } else {
+      // Update timestamps otherwise.
+      var cur = m.source === 'self' ? 0 : 1;
+      totMsgNumber[cur] ++;
+      lastReplyTimestamp[cur] = m.time;
+      if (last !== cur) {
+        if (lastReplyTimestamp[1 - cur] === 0) {
+          continue;
+        }
+        totReplyTime[cur] += m.time - lastReplyTimestamp[1 - cur];
+        cntSession ++;
+        last = cur;
+      }
+    }
+  }
+
+  renderAtId('stats_self', formatDate(totReplyTime[0] / cntSession));
+  renderAtId('stats_opp', formatDate(totReplyTime[1] / cntSession));
+
+  renderAtId('n_self', totMsgNumber[0]);
+  renderAtId('n_opp', totMsgNumber[1]);
+
+  var lastMsg = e.messages[e.messages.length - 1];
+  // Expected reply time should be at least as long as the longest reply time of either side.
+  var expectReplyTime = formatDate(lastMsg.time + (totReplyTime[1] / cntSession) - Date.now());
+
+  if (lastMsg.type === 'separator' || expectReplyTime < 0 || totReplyTime[0] > totReplyTime[1]) {
+    renderAtId('title', 'YES');
+  } else {
+    renderAtId('title', 'NO');
+    renderAtId('suggestion', 'Maybe in ' + expectReplyTime + ' minutes.');
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  getCurrentTabUrl(function(tab) {
+    renderAtId('error', 'Loading...');
+
+    // Get friend id from route
+    if (messagesRegex.test(tab.url)) {
+      chrome.tabs.sendMessage(tab.id, { text: 'getMessages', oppName: tab.url.match(messagesRegex)[1] }, displayCurConvoInfo);
+    } else if (messengerRegex.test(tab.url)) {
+      renderAtId(
+        'error',
+        'Chatting on messenger.com? Check out <a href=\'https://www.facebook.com/messages/' +
+          tab.url.match(messengerRegex)[1] +
+          '\'>here</a> to see if you should reply or not!'
+      );
+    } else {
+      renderAtId('error', 'This extension only works for facebook messages.');
+    }
+  });
+});
+
+document.addEventListener('click', function(e){
+  if (e.target.href !== undefined) {
+    chrome.tabs.create({ url: e.target.href });
+  }
+});
